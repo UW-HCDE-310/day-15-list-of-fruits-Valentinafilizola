15de376b841fcb0d52a7327b8ada038f6c386bcd
Valentina Filizola <Valentina@filizola.com>
2024-11-20 23:37:05 -0800

    finished

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+import keys
 
 app = Flask(__name__)
+
+# Print the secret keys to the console
+print("API Key 1:", keys.MY_SECRET_API_KEY_1)
+print("API Key 2:", keys.MY_SECRET_API_KEY_2)
+
 @app.route("/")
 def index():
-    fruits = [
+    all_fruits = [
         {"name": "apples", "quantity": 3},
-            {"name": "oranges", "quantity": 2},
-            {"name": "strawberries", "quantity": 6}
+        {"name": "oranges", "quantity": 2},
+        {"name": "strawberries", "quantity": 6}
     ]
+   
+    fruits = [fruit for fruit in all_fruits if fruit["name"].startswith("o") and fruit["quantity"] < 3]
     return render_template("index.html", fruits=fruits)
